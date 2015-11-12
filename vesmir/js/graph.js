28d1ea194b0d6865c4d513064b2407ff6a3d8819
Rado Buransky <radoslav.buransky@avitech.aero>
2015-11-12 09:21:06 +0100

    Fullsize D3

--- vesmir/js/graph.js
+++ vesmir/js/graph.js
@@ -1,80 +1,64 @@
-var width = 960,
-    height = 500;
+$(function(){
+    var $container = $('.container'),
+        width = $container.width(),
+        height = $container.height();
 
-var fill = d3.scale.category20();
+    var fill = d3.scale.category20();
 
-var force = d3.layout.force()
-    .size([width, height])
-    .nodes([{}]) // initialize with a single node
-    .linkDistance(30)
-    .charge(-60)
-    .on("tick", tick);
+    var force = d3.layout.force()
+        .size([width, height])
+        .nodes([]) // initialize with a single node
+        .linkDistance(100)
+        .charge(-300)
+        .gravity(0.1)
+        .on("tick", tick);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .on("mousemove", mousemove)
-    .on("mousedown", mousedown);
+    var svg = d3.select(".container").append("svg")
+        .attr("width", '100%')
+        .attr("height", '100%')
+        .attr('viewBox','0 0 '+width+' '+height)
+        .attr('preserveAspectRatio','xMinYMin');
 
-svg.append("rect")
-    .attr("width", width)
-    .attr("height", height);
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height);
 
-var nodes = force.nodes(),
-    links = force.links(),
-    node = svg.selectAll(".node"),
-    link = svg.selectAll(".link");
+    var nodes = force.nodes(),
+        links = force.links(),
+        node = svg.selectAll(".node"),
+        link = svg.selectAll(".link");
 
-var cursor = svg.append("circle")
-    .attr("r", 30)
-    .attr("transform", "translate(-100,-100)")
-    .attr("class", "cursor");
+    n1 = {x: 10, y: 10};
+    n2 = {x: 10, y: 20};
+    n3 = {x: 30, y: 20};
+    nodes.push(n1, n2, n3);
+    links.push({source: n1, target: n2});
+    links.push({source: n1, target: n3});
 
-restart();
+    restart();
 
-function mousemove() {
-  cursor.attr("transform", "translate(" + d3.mouse(this) + ")");
-}
+    function tick() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
 
-function mousedown() {
-  var point = d3.mouse(this),
-      node = {x: point[0], y: point[1]},
-      n = nodes.push(node);
-
-  // add links to any nearby nodes
-  nodes.forEach(function(target) {
-    var x = target.x - node.x,
-        y = target.y - node.y;
-    if (Math.sqrt(x * x + y * y) < 30) {
-      links.push({source: node, target: target});
+      node.attr("cx", function(d) { return d.x; })
+          .attr("cy", function(d) { return d.y; });
     }
-  });
 
-  restart();
-}
+    function restart() {
+      link = link.data(links);
 
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+      link.enter().insert("line", ".node")
+          .attr("class", "link");
 
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
+      node = node.data(nodes);
 
-function restart() {
-  link = link.data(links);
+      node.enter().insert("circle", ".cursor")
+          .attr("class", "node")
+          .attr("r", 5);
 
-  link.enter().insert("line", ".node")
-      .attr("class", "link");
-
-  node = node.data(nodes);
-
-  node.enter().insert("circle", ".cursor")
-      .attr("class", "node")
-      .attr("r", 5)
-      .call(force.drag);
-
-  force.start();
-}
+      force.start();
+    }
+});
