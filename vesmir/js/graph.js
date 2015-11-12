5fa19caef5457dce885b686535185e1b180af48f
Rado Buransky <radoslav.buransky@avitech.aero>
2015-11-12 15:25:56 +0100

    Hide/show label on mouse over

--- vesmir/js/graph.js
+++ vesmir/js/graph.js
@@ -1,5 +1,7 @@
 var TYPE_OPIS = "opis";
 var TYPE_PROJECT = "project";
+var TYPE_COMPANY = "company";
+var TYPE_CONTRACT = "contract";
 
 $(function(){
     var $container = $('#container'),
@@ -33,6 +35,7 @@ $(function(){
 
     nodes.push(opisNode);
 
+    // Load projects
     d3.json("js/projects.json", function(error, json) {
         if (error) return console.warn(error);
 
@@ -49,6 +52,53 @@ $(function(){
         restart();
     });
 
+    // Load companies
+    d3.json("js/companies.json", function(error, json) {
+        if (error) return console.warn(error);
+
+        // Add type to all nodes
+        $.map(json.companies, function(val, i) {
+            val.type = TYPE_COMPANY;
+        });
+
+        $.each(json.companies, function(i, val) {
+            //links.push({source: 0, target: i + 1});
+        });
+
+        nodes.push.apply(nodes, json.companies);
+        restart();
+    });
+
+    // Load contracts
+    d3.json("js/contracts.json", function(error, json) {
+        if (error) return console.warn(error);
+
+        // Add type to all nodes
+        $.map(json.contracts, function(val, i) {
+            val.type = TYPE_CONTRACT;
+        });
+
+        $.each(json.contracts, function(i, val) {
+            var contractIndex = nodes.push(val) - 1;
+
+            // Connect contracts and suppliers
+            var suppliers = $.grep(nodes, function(n) { return (n.type === TYPE_COMPANY) && (val.suppliers.indexOf(n.id) > -1) } );
+            $.each(suppliers, function(j, supplier) {
+                var supplierIndex = nodes.indexOf(supplier);
+                links.push({source: contractIndex, target: supplierIndex});
+            });
+
+            // Connect contracts and projects
+            var projects = $.grep(nodes, function(n) { return (n.type === TYPE_PROJECT) && (val.projects.indexOf(n.id) > -1) } );
+            $.each(projects, function(j, project) {
+                var projectIndex = nodes.indexOf(project);
+                links.push({source: contractIndex, target: projectIndex});
+            });
+        });
+
+        restart();
+    });
+
     var node = svg.selectAll(".node"),
         link = svg.selectAll(".link");
 
@@ -60,6 +110,8 @@ $(function(){
         node = node.data(nodes)
             .enter().append("g")
             .attr("class", "node")
+            .attr("x", 0)
+            .attr("y", 0)
             .call(force.drag);
 
         var circle = node.append("circle")
@@ -73,6 +125,9 @@ $(function(){
             .attr("dy", ".35em")
             .style("display", nodeDisplayStyle)
             .text(nodeText);
+
+        node = svg.selectAll(".node"),
+        link = svg.selectAll(".link");
 
         force.start();
     }
@@ -89,6 +144,8 @@ $(function(){
     function nodeMouseOut(n) {
         switch (n.type) {
             case TYPE_PROJECT:
+            case TYPE_COMPANY:
+            case TYPE_CONTRACT:
                 d3.select(this.parentNode).select("text").style({display: "none"});
         }
     }
@@ -96,16 +153,28 @@ $(function(){
     function nodeMouseOver(n) {
         switch (n.type) {
             case TYPE_PROJECT:
+            case TYPE_COMPANY:
+            case TYPE_CONTRACT:
                 d3.select(this.parentNode).select("text").style({display: "block"});
         }
     }
 
     function nodeText(n) {
+        var formatter = new Intl.NumberFormat('sk-SK', {
+          style: 'currency',
+          currency: 'EUR',
+          minimumFractionDigits: 0,
+        });
+
         switch (n.type) {
             case TYPE_OPIS:
                 return "OPIS";
             case TYPE_PROJECT:
                 return n.name;
+            case TYPE_COMPANY:
+                return n.name;
+            case TYPE_CONTRACT:
+                return formatter.format(n.priceEur);
         }
 
         return "";
@@ -115,22 +184,13 @@ $(function(){
         switch (n.type) {
             case TYPE_OPIS:
                 return 16;
-            case TYPE_PROJECT:
-                return 8;
         }
 
-        return 4.5;
+        return 8;
     }
 
     function nodeFillAttr(n, i) {
-        switch (n.type) {
-            case TYPE_OPIS:
-                return fill(TYPE_OPIS);
-            case TYPE_PROJECT:
-                return fill(TYPE_PROJECT);
-        }
-
-        return fill("");
+        return fill(n.type);
     }
 
     function onTick() {
